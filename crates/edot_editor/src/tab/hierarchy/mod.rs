232ff045b43278a295b53d9301c846c92a6fb2b3
pyhinox <2210267816@qq.com>
2024-09-17 23:49:25 +0800

    hierarchy

--- crates/edot_editor/src/tab/hierarchy/mod.rs
+++ crates/edot_editor/src/tab/hierarchy/mod.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_egui::egui::Ui;
 use edot_tab::prelude::{CommandsExt, TabBuilder};
 
 
@@ -31,6 +32,16 @@ fn setup(
     mut commands:  Commands,
     mut hierarchy: ResMut<Hierarchy>,
 ) {
-    let tab = TabBuilder::new(Name::clone(&hierarchy.tab_name));
+    let tab = TabBuilder::new(Name::clone(&hierarchy.tab_name))
+        .on_show(on_show);
     hierarchy.entity = commands.register_tab(tab).id();
+}
+
+fn on_show(_: Entity, world: &mut World, ui: &mut Ui) {
+    world
+        .query_filtered::<(Entity, Ref<Name>), Without<Parent>>()
+        .iter(world)
+        .for_each(|(_entity, name)| {
+            ui.label(name.as_str());
+        });
 }
